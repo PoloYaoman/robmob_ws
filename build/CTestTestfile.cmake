91637520fdd4697f3bf706a252e7841e1fdd8f4e
arushivisuto <noe.berard-ventura@etu.sorbonne-universite.fr>
2023-11-10 10:14:34 +0100

    "les package fournis par le prof"

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/etudiant/Bureau/robmob_ws/src
+# Build directory: /home/etudiant/Bureau/robmob_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("polo_nono_pkg")
---
+++ build/polo_nono_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/etudiant/Bureau/robmob_ws/src/polo_nono_pkg
+# Build directory: /home/etudiant/Bureau/robmob_ws/build/polo_nono_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
