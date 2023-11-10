5ec8b44d582029bc23cccc29d7398a57d5538820
arushivisuto <noe.berard-ventura@etu.sorbonne-universite.fr>
2023-11-10 10:34:09 +0100

    reset

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/etudiant/Bureau/robmob_ws/src
-# Build directory: /home/etudiant/Bureau/robmob_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("polo_nono_pkg")
--- build/polo_nono_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/etudiant/Bureau/robmob_ws/src/polo_nono_pkg
-# Build directory: /home/etudiant/Bureau/robmob_ws/build/polo_nono_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
