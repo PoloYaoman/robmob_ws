a3b16d824a5070c7757376bbd44d2f0126654eb5
arushivisuto <noe.berard-ventura@etu.sorbonne-universite.fr>
2023-11-10 10:45:32 +0100

    teleop_pkg

---
+++ build/teleop_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/etudiant/robmob_ws/src/teleop_pkg
+# Build directory: /home/etudiant/robmob_ws/build/teleop_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
