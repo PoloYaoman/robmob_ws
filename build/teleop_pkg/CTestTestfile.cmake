846b50397217960043f210a6754048dd58559459
28606017 <olga.shlykova.1@etu.sorbonne-universite.fr>
2023-12-22 10:26:31 +0000

    nav_node

--- build/teleop_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/etudiant/robmob_ws/src/teleop_pkg
-# Build directory: /home/etudiant/robmob_ws/build/teleop_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
