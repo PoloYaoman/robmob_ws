c8b4c1d1af0409e7201333d619dc775472668774
Noé <noe.berard-ventura@etu.sorbonne-universite.fr>
2024-01-24 16:08:26 +0100

    idk

--- src/teleop_pkg/codes/joystick.py
+++ src/teleop_pkg/codes/joystick.py
@@ -3,13 +3,13 @@
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
-rapport = 0.2
+rapport = 0.6
 
 def joystick_callback(data):
     # Créer un message Twist à partir des axes du joystick
     twist = Twist()
     twist.linear.x = data.axes[1]*rapport # Utiliser l'axe vertical pour la vélocité linéaire
-    twist.angular.z = data.axes[0]*rapport  # Utiliser l'axe horizontal pour la vélocité angulaire
+    twist.angular.z = data.axes[0]  # Utiliser l'axe horizontal pour la vélocité angulaire
 
     # Publier le message Twist sur le topic /cmd_vel
     pub.publish(twist)
