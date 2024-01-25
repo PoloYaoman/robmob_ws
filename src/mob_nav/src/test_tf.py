b71283a894ac738a905f20f4c276a5eb1f0790bd
Noé <noe.berard-ventura@etu.sorbonne-universite.fr>
2024-01-25 08:48:20 +0100

    tf tutorial

---
+++ src/mob_nav/src/test_tf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import rospy
+import math
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('robot_tf_listener')
+
+    listener = tf.TransformListener()
+
+    rate = rospy.Rate(10.0)
+    rospy.sleep(1)
+    while not rospy.is_shutdown():
+        try:
+            (trans,rot) = listener.lookupTransform('/base_link', '/map', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+    #Translation en [x,y,z]
+    #Rotation en quaternion
