0f3d8f6c86c123811947f08aa22917b9bf3b2522
Noé <noe.berard-ventura@etu.sorbonne-universite.fr>
2024-01-25 09:44:23 +0100

    publish_point

--- src/mob_nav/src/publish_point.py
+++ src/mob_nav/src/publish_point.py
@@ -2,8 +2,13 @@
 import rospy
 from geometry_msgs.msg import PointStamped
 
-
+def point_callback(data):
+    x = data.point.x
+    y = data.point.y
+    z = data.point.z
+    rospy.loginfo("coordinates:x=%f y=%f z=%f" %(x,y,z))
 
 if __name__ == '__main__':
     rospy.init_node('publish_point_listener')
     rospy.Subscriber("clicked_point", PointStamped, point_callback)
+    rospy.spin()
